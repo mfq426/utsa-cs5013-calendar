5488aa63c7f41bb9f63995a0d6a6d1a57a9ba022
liulou1985@gmail.com <liulou1985@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-10-29 22:42:56 +0000

    Modify MainActivity, and add four view Activities(Daily, Weekly, Monthly, Agenda).

--- trunk/Calendar/src/edu/utsa/calendar/MainActivity.java
+++ trunk/Calendar/src/edu/utsa/calendar/MainActivity.java
@@ -2,14 +2,48 @@ package edu.utsa.calendar;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	
+	public static final int DAILY_VIEW_MODE = 0;
+	public static final int WEEKLY_VIEW_MODE = 1;
+	public static final int MONTHLY_VIEW_MODE = 2;
+	public static final int AGENDA_VIEW_MODE = 3;
+	
+	private SharedPreferences mPrefs;
+	private int mCurViewMode;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		
+		// go to the last view user stayed
+		mPrefs = getSharedPreferences("view", 0);
+		mCurViewMode = mPrefs.getInt("view_mode", DAILY_VIEW_MODE);
+		
+		Intent firstView;
+		switch(mCurViewMode) {
+			case DAILY_VIEW_MODE:
+				firstView = new Intent(this, DailyViewActivity.class);
+				break;
+			case WEEKLY_VIEW_MODE:
+				firstView = new Intent(this, WeeklyViewActivity.class);
+				break;
+			case MONTHLY_VIEW_MODE:
+				firstView = new Intent(this, MonthlyViewActivity.class);
+				break;
+			case AGENDA_VIEW_MODE:
+				firstView = new Intent(this, AgendaViewActivity.class);
+				break;
+			default:
+				System.out.println("Error");
+				return;
+		
+		}
+		
+		startActivity(firstView);
 	}
 
 	@Override
