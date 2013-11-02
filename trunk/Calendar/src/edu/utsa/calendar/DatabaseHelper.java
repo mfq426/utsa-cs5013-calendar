ee726a446461c976825f154b306f0af287705db3
mejbah.alam@gmail.com <mejbah.alam@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-11-02 22:54:12 +0000

    DatabaseHelper class creates table and has methods that returns database references

---
+++ trunk/Calendar/src/edu/utsa/calendar/DatabaseHelper.java
@@ -0,0 +1,73 @@
+package edu.utsa.calendar;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+	
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "CalenderDb";
+    private static final String EVENT_TABLE_NAME = "Events";
+    private static final String CATEGORY_TABLE_NAME = "Events";
+//    private static final String DICTIONARY_TABLE_CREATE =
+//                "CREATE TABLE " + EVENT_TABLE_NAME + " (" +
+//                KEY_WORD + " TEXT, " +
+//                KEY_DEFINITION + " TEXT);";
+//  "Events" Table Columns names
+    private static final String EVENTS_KEY_ID = "event_id";
+    private static final String EVENTS_START_TIME = "start_time"; //TODO: what should be the data type for data ?
+    private static final String EVENTS_END_TIME = "end_time";//TODO: what should be the data type for data ?
+    private static final String EVENTS_DESCRIPTION = "details";//TODO: what should be the data type for data ?
+ 
+//  "Category" Table Columns names
+    private static final String CATEGORY_KEY_ID = "event_id";
+    private static final String CATEGORY_COLOR = "start_time"; //TODO: what should be the data type for data ?
+    private static final String CATEGORY_TYPE = "end_time";//TODO: what should be the data type for data ?
+    private static final String CATEGORY_DESCRIPTION = "details";//TODO: what should be the data type for data ?
+
+    DatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+    
+    /**
+     * Create Two Tables : Events and Categories 
+     */
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		String CREATE_EVENTS_TABLE = "CREATE TABLE " + EVENT_TABLE_NAME + "("
+                + EVENTS_KEY_ID + " INTEGER PRIMARY KEY," + EVENTS_DESCRIPTION + " TEXT,"
+                + EVENTS_START_TIME + " TEXT" + EVENTS_END_TIME + " TEXT"+ ")";
+        db.execSQL(CREATE_EVENTS_TABLE);
+        
+        String CREATE_CATEGORIES_TABLE = "CREATE TABLE " + CATEGORY_TABLE_NAME + "("
+                + CATEGORY_KEY_ID + " INTEGER PRIMARY KEY," + CATEGORY_COLOR + " TEXT,"
+                + CATEGORY_TYPE + " TEXT" + CATEGORY_DESCRIPTION + " TEXT"+ ")";
+        db.execSQL(CREATE_CATEGORIES_TABLE);
+		
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// Drop older table if existed
+        db.execSQL("DROP TABLE IF EXISTS " + EVENT_TABLE_NAME);
+        db.execSQL("DROP TABLE IF EXISTS " + CATEGORY_TABLE_NAME);
+ 
+        // Create tables again
+        onCreate(db);
+		
+	}
+	
+	public SQLiteDatabase getDatabaseForRead() {
+		return this.getReadableDatabase();
+	}
+	
+	public SQLiteDatabase getDatabaseForWrite() {
+		return this.getWritableDatabase();
+	}
+	
+	
+	
+
+}
