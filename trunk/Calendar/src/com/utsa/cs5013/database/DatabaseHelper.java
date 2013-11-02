e852ed01b3f336b83b81fe8b909bf3b0488182ca
nahid.mos@gmail.com <nahid.mos@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-11-02 01:01:03 +0000

    -Database added

---
+++ trunk/Calendar/src/com/utsa/cs5013/database/DatabaseHelper.java
@@ -0,0 +1,66 @@
+package com.utsa.cs5013.database;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+
+import android.R.anim;
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+	public static final String TABLE_EVENT_NAME = "Event";
+	public static final String TABLE_EVENT_FILE = "table_event.sqlite";
+	public static final String TABLE_CATEGORY_NAME = "Category";
+	public static final String TABLE_CATEGORY_FILE = "table_category.sqlite";
+	private static final String DATABASE_NAME = "calendar.db";
+	private static final int DATABASE_VERSION = 1;
+	private Context mContext = null;
+	private String currentTable;
+
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		mContext = context;
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase database) {
+		database.execSQL(createTableQuery(mContext, TABLE_EVENT_FILE));
+		database.execSQL(createTableQuery(mContext, TABLE_CATEGORY_FILE));
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		Log.w(DatabaseHelper.class.getName(), "Upgrading database from version " + oldVersion + " to " + newVersion
+				+ ", which will destroy all old data");
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_EVENT_NAME);
+		db.execSQL("DROP TABLE IF EXISTS " + TABLE_CATEGORY_NAME);
+		onCreate(db);
+	}
+
+	private String createTableQuery(Context context, String fileName) {
+		AssetManager assets = context.getAssets();
+		StringBuffer buffer = new StringBuffer();
+		try {
+			InputStream reader = assets.open(fileName);
+
+			int data = reader.read();
+			while (data != -1) {
+				char theChar = (char) data;
+				buffer.append(theChar);
+				data = reader.read();
+			}
+
+			reader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return buffer.toString();
+	}
+}
---
+++ trunk/Calendar/src/com/utsa/cs5013/entity/Event.java
@@ -0,0 +1,55 @@
+package com.utsa.cs5013.entity;
+
+import java.lang.reflect.Field;
+import java.util.Date;
+
+public class Event {
+	private int mID;
+	private Date mStartDate;
+	private Date mEndDate;
+	private int mCategoryID;
+	private String mDescription;
+
+	public int getID() {
+		return mID;
+	}
+
+	public void setID(int pID) {
+		mID = pID;
+	}
+
+	public Date getStartDate() {
+		return mStartDate;
+	}
+
+	public void setStartDate(Date pStartDate) {
+		mStartDate = pStartDate;
+	}
+
+	public Date getEndDate() {
+		return mEndDate;
+	}
+
+	public void setEndDate(Date pEndDate) {
+		mEndDate = pEndDate;
+	}
+
+	public int getCategoryID() {
+		return mCategoryID;
+	}
+
+	public void setCategoryID(int pCategoryID) {
+		mCategoryID = pCategoryID;
+	}
+
+	public String getDescription() {
+		return mDescription;
+	}
+
+	public void setDescription(String pDescription) {
+		mDescription = pDescription;
+	}
+
+	
+	
+}
