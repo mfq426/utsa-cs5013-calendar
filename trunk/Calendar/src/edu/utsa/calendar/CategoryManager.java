c209886b21893a2cc24acedef621d69c52a3d489
mejbah.alam@gmail.com <mejbah.alam@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-11-10 00:21:51 +0000

    Category entity Class created. Category Manager Class added with functionalities added to perform required operation on category table in Database

--- trunk/Calendar/src/edu/utsa/calendar/CategoryManager.java
+++ trunk/Calendar/src/edu/utsa/calendar/CategoryManager.java
@@ -1,8 +1,10 @@
 package edu.utsa.calendar;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 
@@ -15,20 +17,129 @@ public class CategoryManager implements Serializable{
 		super();
 		this.storageHandler = storageHandler;
 	}
-
-	public void createCategory(String mName, int mColor, String mDescription){
+	
+	/**
+	 * Add a new/conflicting category to the database
+	 * @param category
+	 */
+	public void addCategory( Category c ){
 		
 		SQLiteDatabase db = storageHandler.getWritableDatabase();
 		 
+		
 		ContentValues values = new ContentValues();
-		//values.put(DatabaseHelper., mName); // Contact Name
-		//values.put(KEY_COLOR, mColor);
-		//values.put(KEY_DESCRIPTION, mDescription);// Contact Phone Number
-		 
+		values.put(storageHandler.getCategoryType(), c.getName());
+		values.put(storageHandler.getCategoryColor(), c.getColor());
+		values.put(storageHandler.getCategoryDescription(), c.getDescription());
+				 
 		// Inserting Row
-		//db.insert(, null, values);
-		db.close(); // Closing database connection
+		db.insert(storageHandler.getCategoryTableName(), null, values);
+		db.close(); // Closing database connection		db.close(); // Closing database connection
 			
 	
 	}
+	
+	/**
+	 * Delete the category in DB with the same name
+	 * @param Category object with all field initialized
+	 */
+	
+	public void deleteCategory( Category c ){
+		
+		SQLiteDatabase db = storageHandler.getWritableDatabase();
+		 
+		// Deleting Row
+		db.delete(storageHandler.getCategoryTableName(), 
+	            storageHandler.getCategoryType() + "= " + c.getName(), 
+	            null);
+		db.close(); // Closing database connection		db.close(); // Closing database connection
+			
+	
+	}
+	
+	/**
+	 * Delete the category in DB with "name"
+	 * @param Category object with all field initialized
+	 */
+	
+	public void deleteCategory( String name ){
+		
+		SQLiteDatabase db = storageHandler.getWritableDatabase();
+		 
+		// Deleting Row
+		db.delete(storageHandler.getCategoryTableName(), 
+	            storageHandler.getCategoryType() + "= " + name, 
+	            null);
+		db.close(); // Closing database connection		db.close(); // Closing database connection
+			
+	
+	}
+	
+	/**
+	 * Delete the category in DB with color = "color"
+	 * @param Category object with all field initialized
+	 */
+	
+	public void deleteCategory( int color ){
+		
+		SQLiteDatabase db = storageHandler.getWritableDatabase();
+		 
+		// Deleting Row
+		db.delete(storageHandler.getCategoryTableName(), 
+	            storageHandler.getCategoryColor() + "= " + color, 
+	            null);
+		db.close(); // Closing database connection		db.close(); // Closing database connection
+			
+	
+	}
+	
+	/**
+	 *Reads the categories in database has name = "name" 
+	 * @param name/type of the categoy
+	 * @return arraylist of categories with type "name", list size is 0 if no such category
+	 */
+	public ArrayList<Category> readCategory( String name ) {
+		ArrayList<Category>categoryList = new ArrayList<Category>();
+		String selectQuery = "SELECT  * FROM " + storageHandler.getCategoryTableName()+ 
+				" WHERE " +storageHandler.getCategoryType()+ " = " + "\"" + name + "\"";
+		SQLiteDatabase db = storageHandler.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+		if (cursor.moveToFirst()) {
+            do {
+			
+            	Category c = new Category( Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(1)), name, cursor.getString(3));
+			
+            	categoryList.add(c);
+            } while(cursor.moveToNext());
+		}
+		
+		return categoryList;
+		
+	}
+	
+	/**
+	 *Reads the categories in database has name = "name" 
+	 * @param name/type of the categoy
+	 * @return arraylist of categories with type "name", list size is 0 if no such category
+	 */
+	public ArrayList<Category> readCategory( int color ) {
+		ArrayList<Category>categoryList = new ArrayList<Category>();
+		String selectQuery = "SELECT  * FROM " + storageHandler.getCategoryTableName()+ 
+				" WHERE " +storageHandler.getCategoryType()+ " = " + "\"" + color + "\"";
+		SQLiteDatabase db = storageHandler.getWritableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);
+		if (cursor.moveToFirst()) {
+            do {
+			
+            	Category c = new Category( Integer.parseInt(cursor.getString(0)), Integer.parseInt(cursor.getString(1)), cursor.getString(2), cursor.getString(3));
+			
+            	categoryList.add(c);
+            } while(cursor.moveToNext());
+		}
+		
+		return categoryList;
+		
+	}
+	
+	
 }
