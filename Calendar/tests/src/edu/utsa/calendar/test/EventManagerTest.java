595c7e88d0ce405d27e2c2249c63aeddfa8936af
nahid.mos@gmail.com <nahid.mos@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-11-12 07:03:26 +0000

    -Test case added - anyone now can work on that

---
+++ Calendar/tests/src/edu/utsa/calendar/test/EventManagerTest.java
@@ -0,0 +1,68 @@
+package edu.utsa.calendar.test;
+
+import java.util.Calendar;
+import java.util.List;
+
+import edu.utsa.calendar.Category;
+import edu.utsa.calendar.CategoryManager;
+import edu.utsa.calendar.DatabaseHelper;
+import edu.utsa.calendar.Event;
+import edu.utsa.calendar.EventManager;
+import android.test.AndroidTestCase;
+
+public class EventManagerTest extends AndroidTestCase {
+	private DatabaseHelper mDatabaseHelper = null;
+	private CategoryManager mCategoryManager = null;
+	private EventManager mEventManager = null;
+	@Override
+	public void setUp() throws Exception {
+		super.setUp();
+		mDatabaseHelper = new DatabaseHelper(getContext());
+		mCategoryManager = new CategoryManager(mDatabaseHelper);
+		mEventManager = new EventManager(mDatabaseHelper, mCategoryManager);
+		
+	}
+
+	@Override
+	protected void tearDown() throws Exception {
+		mDatabaseHelper.close();
+
+		super.tearDown();
+	}
+
+	public void testCreateEvent() throws Exception {
+		Calendar startDate = Calendar.getInstance();
+		Calendar endDate = Calendar.getInstance();
+		startDate.set(Calendar.HOUR_OF_DAY, 7);
+		startDate.set(Calendar.MINUTE, 30);
+		
+		endDate.set(Calendar.HOUR_OF_DAY, 7);
+		endDate.set(Calendar.MINUTE, 50);
+		Event event = new Event(startDate,endDate,"12"," Class", 1,1);
+		mEventManager.createEvent(event);
+		List<Event> readEvents = mEventManager.readEvents(startDate, endDate);
+		
+		assertEquals(1,readEvents.size());
+		assertEquals("Class",readEvents.get(0).getDescription());
+	}
+	
+	public void testDeleteEvent() throws Exception {
+		Calendar startDate = Calendar.getInstance();
+		Calendar endDate = Calendar.getInstance();
+		startDate.set(Calendar.HOUR_OF_DAY, 7);
+		startDate.set(Calendar.MINUTE, 30);
+		
+		endDate.set(Calendar.HOUR_OF_DAY, 7);
+		endDate.set(Calendar.MINUTE, 50);
+		
+		mEventManager.deleteEvent(startDate, endDate);
+		List<Event> readEvents = mEventManager.readEvents(startDate, endDate);
+		
+		assertEquals(0,readEvents.size());
+		
+	}
+	
+
+
+	
+}
