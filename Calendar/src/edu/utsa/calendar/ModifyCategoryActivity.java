fd5f9020ecf088174fbec1686373fbee5712626a
liulou1985@gmail.com <liulou1985@gmail.com@79cc6040-3ffa-082e-083d-78d503296c50>
2013-11-17 02:08:54 +0000

    move characters of category user interface from hard-coded to resource file

--- Calendar/src/edu/utsa/calendar/ModifyCategoryActivity.java
+++ Calendar/src/edu/utsa/calendar/ModifyCategoryActivity.java
@@ -20,16 +20,11 @@ import android.widget.TextView;
 
 /**
  * ModifyCategoryActivity provides user the capability of removing category
+ * 
  * @author Lu Liu
  */
 public class ModifyCategoryActivity extends Activity {
-	
-	private static final String PROMPT = "select category to delete";
-	private static final String INFO = "No category to delete";
-	private static final String DELETE = "delete";
-	private static final String RETURN = "return";
-	private static final String PROMPT2 = "please select category";
-	private static final String DEFAULT_CATEGORY = "default";
+
 	private static final int ONE = 1;
 	private static final int TWO = 2;
 	private static final int THREE = 3;
@@ -40,70 +35,87 @@ public class ModifyCategoryActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_modify_category);
-		
-		final CategoryManager manager = Manager.getInstance().getCategoryManager();
+
+		final CategoryManager manager = Manager.getInstance()
+				.getCategoryManager();
 		ArrayList<Category> list = manager.readAllCategory();
-		
+		final String PROMPT = getResources().getString(
+				R.string.select_category_to_delete);
+		final String INFO = getResources().getString(
+				R.string.info);
+		final String DELETE = getResources().getString(
+				R.string.delete);
+		final String RETURN = getResources().getString(
+				R.string.cancel);
+		final String PROMPT2 = getResources().getString(
+				R.string.prompt);;
+		final String DEFAULT_CATEGORY = "default";
+
 		RelativeLayout layout = (RelativeLayout) findViewById(R.id.delete_category_view);
 		ArrayList<String> options = new ArrayList<String>();
 		Category c;
 		String s;
-		
+
 		// check if is there any category except the default category
-		if(list.size() > 1) {
+		if (list.size() > 1) {
 			Iterator<Category> itr = list.iterator();
-			while(itr.hasNext()) {
+			while (itr.hasNext()) {
 				c = itr.next();
 				s = c.getName();
-				if(!(s.equalsIgnoreCase(DEFAULT_CATEGORY))) {
+				if (!(s.equalsIgnoreCase(DEFAULT_CATEGORY))) {
 					options.add(s);
 				}
 			}
-			
-			RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			RelativeLayout.LayoutParams params4 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+			RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			RelativeLayout.LayoutParams params3 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			RelativeLayout.LayoutParams params4 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
 			// add a textview to the relative layout dynamically
 			TextView textview = new TextView(this);
 			textview.setId(ONE);
 			textview.setText(PROMPT);
 			params1.addRule(RelativeLayout.CENTER_HORIZONTAL);
 			layout.addView(textview, params1);
-			
+
 			// add a spinner to the relative layout dynamically
 			Spinner spinner = new Spinner(this);
 			spinner.setId(TWO);
-			ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, options);
+			ArrayAdapter<CharSequence> adapter = new ArrayAdapter(this,
+					android.R.layout.simple_spinner_item, options);
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			spinner.setAdapter(adapter);
 			spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
-					@Override
-					public void onItemSelected(AdapterView<?> parent, View view,
-							int pos, long id) {
-						// TODO Auto-generated method stub
-						categoryName = (String)parent.getItemAtPosition(pos);
-					}
+				@Override
+				public void onItemSelected(AdapterView<?> parent, View view,
+						int pos, long id) {
+					// TODO Auto-generated method stub
+					categoryName = (String) parent.getItemAtPosition(pos);
+				}
 
-					@Override
-					public void onNothingSelected(AdapterView<?> arg0) {
-						// TODO Auto-generated method stub
-						
-					}
+				@Override
+				public void onNothingSelected(AdapterView<?> arg0) {
+					// TODO Auto-generated method stub
+
+				}
 			});
 			params2.addRule(RelativeLayout.CENTER_HORIZONTAL);
 			params2.addRule(RelativeLayout.BELOW, textview.getId());
 			layout.addView(spinner, params2);
-			
+
 			// add a create button to the relative layout dynamically
 			Button create_button = new Button(this);
 			create_button.setId(THREE);
 			create_button.setText(DELETE);
 			create_button.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
-					if(categoryName == null || categoryName.isEmpty()) {
+					if (categoryName == null || categoryName.isEmpty()) {
 						popup(PROMPT2);
 					} else {
 						manager.deleteCategory(categoryName);
@@ -114,28 +126,30 @@ public class ModifyCategoryActivity extends Activity {
 			params3.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 			params3.addRule(RelativeLayout.ALIGN_PARENT_LEFT);
 			layout.addView(create_button, params3);
-			
+
 			// add a return button to the relative layout dynamically
 			Button return_button = new Button(this);
 			return_button.setId(FOUR);
 			return_button.setText(RETURN);
 			return_button.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					finish();
-					
+
 				}
 			});
 			params4.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 			params4.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 			layout.addView(return_button, params4);
-			
+
 		} else {
 			// there is no other category except default category
-			RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
-			
+			RelativeLayout.LayoutParams params1 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+			RelativeLayout.LayoutParams params2 = new RelativeLayout.LayoutParams(
+					LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
 			// add a textview to the relative layout dynamically
 			TextView textview = new TextView(this);
 			textview.setId(ONE);
@@ -149,18 +163,18 @@ public class ModifyCategoryActivity extends Activity {
 			params2.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 			params2.addRule(RelativeLayout.ALIGN_PARENT_RIGHT);
 			return_button.setOnClickListener(new View.OnClickListener() {
-				
+
 				@Override
 				public void onClick(View v) {
 					finish();
 				}
 			});
-			
+
 			layout.addView(textview, params1);
 			layout.addView(return_button, params2);
 		}
 	}
-	
+
 	// give user feedback when something goes wrong
 	private void popup(CharSequence text) {
 		Context context = getApplicationContext();
@@ -172,7 +186,6 @@ public class ModifyCategoryActivity extends Activity {
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.modify_category, menu);
 		return true;
 	}
--- Calendar/src/edu/utsa/calendar/NewCategoryActivity.java
+++ Calendar/src/edu/utsa/calendar/NewCategoryActivity.java
@@ -1,13 +1,10 @@
 package edu.utsa.calendar;
 
 import java.util.ArrayList;
-
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -15,63 +12,71 @@ import android.widget.Toast;
 import com.larswerkman.holocolorpicker.ColorPicker;
 
 /**
- * NewCategoryActivity in responsible of create category on user's demand
+ * NewCategoryActivity in responsible of creating category on user's demand
+ * 
  * @author Lu Liu
  */
 public class NewCategoryActivity extends Activity {
-	
+
 	private String name;
 	private String description;
 	private int color;
-	
-	private final static String NAME_IS_TAKEN = "category name is taken";
-	private final static String INCOMPLETE_USER_INPUT = "incomplete user input";
+
+	private String NAME_IS_TAKEN;
+	private String INCOMPLETE_USER_INPUT;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
+		NAME_IS_TAKEN = getResources().getString(
+				R.string.name_is_taken);
+		INCOMPLETE_USER_INPUT = getResources().getString(
+				R.string.incomplte_input);
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_new_category);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.new_category, menu);
 		return true;
 	}
-	
+
 	// collect user inputs
 	private void getData() {
-		EditText editText = (EditText)findViewById(R.id.name_text);
+		EditText editText = (EditText) findViewById(R.id.name_text);
 		name = editText.getText().toString();
-		
-		editText = (EditText)findViewById(R.id.description_text);
+
+		editText = (EditText) findViewById(R.id.description_text);
 		description = editText.getText().toString();
-		
+
 		ColorPicker picker = (ColorPicker) findViewById(R.id.color_picker);
 		color = picker.getColor();
 	}
-	
+
 	// verify user inputs
 	private boolean verifyData() {
-		if(name == null || name.isEmpty() || description == null || description.isEmpty()) {
+		if (name == null || name.isEmpty() || description == null
+				|| description.isEmpty()) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	}
-	
+
 	/**
-	 * create category by go through getting user inputs, verifying user inputs and resolving category name conflict
-	 * @param creat category button
+	 * create category by going through getting user inputs, verifying user inputs
+	 * and resolving category name conflict
+	 * 
+	 * @param view
+	 *            create category button
 	 */
 	public void createCategory(View view) {
 		getData();
-		if(verifyData()) {
-			CategoryManager manager = Manager.getInstance().getCategoryManager();
+		if (verifyData()) {
+			CategoryManager manager = Manager.getInstance()
+					.getCategoryManager();
 			ArrayList<Category> list = manager.readCategory(name);
-			if(list.size()>0) {
+			if (list.size() > 0) {
 				popup(NAME_IS_TAKEN);
 			} else {
 				Category c = new Category(color, name, description);
@@ -82,7 +87,7 @@ public class NewCategoryActivity extends Activity {
 			popup(INCOMPLETE_USER_INPUT);
 		}
 	}
-	
+
 	// give user feedback when something goes wrong
 	private void popup(CharSequence text) {
 		Context context = getApplicationContext();
@@ -91,10 +96,12 @@ public class NewCategoryActivity extends Activity {
 		Toast toast = Toast.makeText(context, text, duration);
 		toast.show();
 	}
-	
+
 	/**
 	 * return to the last activity
-	 * @param return button
+	 * 
+	 * @param view
+	 *            return button
 	 */
 	public void cancel(View view) {
 		finish();
